87d60ef41011a86839b6bec6b8417b7fdbebd5f0
petrprvnisoukromy <119962290+petrprvnisoukromy@users.noreply.github.com>
2024-08-29 17:50:36 +0200

    naprava

--- repository-root/netlify/functions/sendEmail.js
+++ repository-root/netlify/functions/sendEmail.js
@@ -1,27 +1,26 @@
-const emailjs = require('emailjs'); // Assuming you're using emailjs in the serverless function
+const emailjs = require('emailjs');
 
 exports.handler = async function(event, context) {
-    const { name, email, message } = JSON.parse(event.body);
+    const { name, email} = JSON.parse(event.body);
 
     const emailParams = {
         to_name: name,
         to_email: email, // This will send the email to the user's email address
-        message: message,
     };
 
     try {
-        // Sending email using EmailJS
-        await emailjs.send('service_67czw3m', 'template_o51od7c', emailParams, 'WeTdS5lsV21zN9PSO');
+        console.log('Sending email with params:', emailParams);
+        await emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', emailParams, 'YOUR_USER_ID');
         
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'Email sent successfully' }),
         };
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error occurred while sending email:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: 'Failed to send email' }),
+            body: JSON.stringify({ message: 'Failed to send email', error: error.message }),
         };
     }
 };
