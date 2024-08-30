714de9626b20312298728ed624866794a5613d36
petrprvnisoukromy <119962290+petrprvnisoukromy@users.noreply.github.com>
2024-08-30 13:46:15 +0200

    oprava more

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,37 +1,58 @@
-const emailjs = require('emailjs-com');
+const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
+    console.log("Function triggered");
     try {
         if (event.httpMethod !== 'POST') {
+            console.log("Invalid HTTP method:", event.httpMethod);
             return {
                 statusCode: 405,
                 body: JSON.stringify({ message: 'Method Not Allowed' }),
             };
         }
 
-        console.log('Received event:', event.body);  // Log the incoming data
+        console.log('Event body:', event.body);
 
-        const EMAILJS_USER_ID = 'WeTdS5lsV21zN9PSO';  // Replace with your actual EmailJS user ID
-        const EMAILJS_SERVICE_ID = 'service_67czw3m';  // Replace with your actual EmailJS service ID
-        const EMAILJS_TEMPLATE_ID = 'template_o51od7c';  // Replace with your actual EmailJS template ID
+        const EMAILJS_USER_ID = process.env.EMAILJS_USER_ID;
+        const EMAILJS_SERVICE_ID = process.env.EMAILJS_SERVICE_ID;
+        const EMAILJS_TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID;
 
         const data = JSON.parse(event.body);
+        console.log('Parsed data:', data);
 
-        console.log('Sending email with:', EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, EMAILJS_USER_ID, data);
-
-        const response = await emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, {
-            to_name: 'Test User',
-            from_name: "Haze Haven",
-            to_email: 'test@example.com',
-        }, EMAILJS_USER_ID);
-        
-
-        console.log('EmailJS response:', response);
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Email sent successfully' }),
+        const emailData = {
+            service_id: EMAILJS_SERVICE_ID,
+            template_id: EMAILJS_TEMPLATE_ID,
+            user_id: EMAILJS_USER_ID,
+            template_params: {
+                to_name: data.name,
+                to_email: data.email,
+                from_name: "Haze Haven",
+            }
         };
+
+        const response = await fetch('https://api.emailjs.com/api/v1.0/email/send', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(emailData)
+        });
+
+        if (response.ok) {
+            console.log('Email sent successfully');
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: 'Email sent successfully' }),
+            };
+        } else {
+            console.log('Failed to send email');
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: 'Failed to send email' }),
+            };
+        }
+
     } catch (error) {
         console.error('Error sending email:', error.message);
         return {
