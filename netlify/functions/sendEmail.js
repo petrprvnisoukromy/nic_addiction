1e9f1d38a2e7e3959811572e26e321c5c465d236
petrprvnisoukromy <119962290+petrprvnisoukromy@users.noreply.github.com>
2024-08-30 12:49:05 +0200

    dsdjsdjsdj

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -9,13 +9,25 @@ exports.handler = async function(event, context) {
             };
         }
 
+        // Ensure the environment variables are correctly set
+        const EMAILJS_USER_ID = process.env.EMAILJS_USER_ID;
+        const EMAILJS_SERVICE_ID = process.env.EMAILJS_SERVICE_ID;
+        const EMAILJS_TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID;
+
+        if (!EMAILJS_USER_ID || !EMAILJS_SERVICE_ID || !EMAILJS_TEMPLATE_ID) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: 'EmailJS environment variables are not set correctly.' }),
+            };
+        }
+
         const data = JSON.parse(event.body);
 
-        const response = await emailjs.send("service_67czw3m", "template_o51od7c", {
+        const response = await emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, {
             to_name: data.name || 'Unknown',
             from_name: "Haze Haven",
             to_email: data.email || 'No email provided',
-        });
+        }, EMAILJS_USER_ID);
 
         console.log('EmailJS response:', response);
 
@@ -28,6 +40,6 @@ exports.handler = async function(event, context) {
         return {
             statusCode: 500,
             body: JSON.stringify({ message: 'Failed to send email', error: error.message }),
-        };
+        };aá
     }
 };
